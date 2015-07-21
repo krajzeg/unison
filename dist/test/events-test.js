e906fd1e43fdf25bd0d841a18b496ba2256b72db
Jakub Wasilewski <wasyl@wasyl.eu>
2015-07-22 00:00:09 +0200

    Improved events test a little bit.

--- dist/test/events-test.js
+++ dist/test/events-test.js
@@ -49,18 +49,21 @@ describe('When children are added', function () {
 
 
   it('\'childAdded\' and \'created\' events should trigger for nested objects', function () {
-    var created = sinon.spy(), childAdded = sinon.spy(), deepCreated = sinon.spy();
+    var created = sinon.spy(), childAdded = sinon.spy(), 
+    deepCreated = sinon.spy(), deepChildAdded = sinon.spy();
 
     $$('food.apple.seed.inside').on('created', deepCreated);
     $$('food.apple.seed.outside').on('created', deepCreated);
     $$('food.apple.seed').on('created', created);
     $$('food.apple').on('childAdded', childAdded);
+    $$('food.apple.seed').on('childAdded', deepChildAdded);
 
     $$('food').add('apple', { seed: { inside: {}, outside: {} } });
 
     assert.ok(created.calledOnce);
     assert.ok(childAdded.calledOnce);
-    assert.ok(deepCreated.calledTwice);});});
+    assert.ok(deepCreated.calledTwice);
+    assert.ok(deepChildAdded.calledTwice);});});
 
 
 
--- test/events-test.js
+++ test/events-test.js
@@ -49,18 +49,21 @@ describe("When children are added", () => {
   });
 
   it("'childAdded' and 'created' events should trigger for nested objects", () => {
-    let created = sinon.spy(), childAdded = sinon.spy(), deepCreated = sinon.spy();
+    let created = sinon.spy(), childAdded = sinon.spy(),
+      deepCreated = sinon.spy(), deepChildAdded = sinon.spy();
 
     $$('food.apple.seed.inside').on('created', deepCreated);
     $$('food.apple.seed.outside').on('created', deepCreated);
     $$('food.apple.seed').on('created', created);
     $$('food.apple').on('childAdded', childAdded);
+    $$('food.apple.seed').on('childAdded', deepChildAdded);
 
     $$('food').add('apple', { seed: { inside: {}, outside: {} } });
 
     assert.ok(created.calledOnce);
     assert.ok(childAdded.calledOnce);
     assert.ok(deepCreated.calledTwice);
+    assert.ok(deepChildAdded.calledTwice);
   });
 });
 
